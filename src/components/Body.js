005c7b5a025d12b8dba31a1667fd50065854310e
kriteshmoyal01 <kartikdewani01@gmail.com>
2026-05-06 21:33:47 +0530

    shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [list, setList] = useState(resList);
+
+useEffect(() => {
+  fetchData();
+},[]);
+
+const fetchData = async() =>{
+  const data = await fetch (
+    "https://www.swiggy.com/mapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&carousel=true&third_party_vendor=1"
+  );
+
+  const json = await data.json();
+  console.log(json);
+
+ // setList(json.data.cards);\
+
+};
+
+if (list.length ===0){
+  return <Shimmer/>
+}
 
   return (
     <div className="body">
